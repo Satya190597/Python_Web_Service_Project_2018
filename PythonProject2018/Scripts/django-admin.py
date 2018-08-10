5431cd4cf998fa0e83b06336ed60ffdb983394ed
satya19051997 <satya.nandy@evossolutions.com>
2018-08-10 18:55:39 +0530

    Initial Commit

---
+++ PythonProject2018/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!f:\my git hub\python_web_service_project_2018\pythonproject2018\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
