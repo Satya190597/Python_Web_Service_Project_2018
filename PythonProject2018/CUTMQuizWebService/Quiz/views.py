4366586272eaa28ac49af2da43f63f30e82694e0
satya19051997 <satya.nandy@evossolutions.com>
2018-08-13 00:43:12 +0530

    Adding app view for QuestionBank To Insert And Get

--- PythonProject2018/CUTMQuizWebService/Quiz/views.py
+++ PythonProject2018/CUTMQuizWebService/Quiz/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import QuestionBank
+from .serializers import QuestionBankSerializer
 
-# Create your views here.
+@api_view(['GET','POST'])
+def questionBank_list(request):
+
+    if request.method == 'GET':
+        questionBank = QuestionBank.all()
+        serializer = QuestionBankSerializer(questionBank, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = QuestionBankSerializer(data=request.data)
+        if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data,status=status.HTTP_201_CREATED)
