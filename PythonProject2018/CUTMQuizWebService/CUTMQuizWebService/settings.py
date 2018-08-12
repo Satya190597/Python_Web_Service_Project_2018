44d90749e5f10dcc7661284f13e04549df324fd2
satya19051997 <satya.nandy@evossolutions.com>
2018-08-13 00:19:55 +0530

    Question Bank Model

--- PythonProject2018/CUTMQuizWebService/CUTMQuizWebService/settings.py
+++ PythonProject2018/CUTMQuizWebService/CUTMQuizWebService/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Quiz.apps.QuizConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- PythonProject2018/CUTMQuizWebService/Quiz/models.py
+++ PythonProject2018/CUTMQuizWebService/Quiz/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+# Question Bank Model
+class QuestionBank(models.Model):
+    question = models.CharField(max_length=250)
+    category = models.CharField(max_length=250)
+    option_one = models.CharField(max_length=250)
+    option_two = models.CharField(max_length=250)
+    option_three = models.CharField(max_length=250)
+    option_four = models.CharField(max_length=250)
+    ans = models.CharField(max_length=250)
